7edbfe94a4ea76452bdf5584dd8781bd6ebcbf24
Yeu07 <yeumensilva9@gmail.com>
2023-08-29 23:16:29 -0300

    Boxplot

--- tp3-busquedas-no-informadas/code/main.py
+++ tp3-busquedas-no-informadas/code/main.py
@@ -2,6 +2,7 @@ import math
 import random
 from environment import Environment
 from searchAlgorithms import *
+import matplotlib.pyplot as plt
 
 def randomPos(max):
     return random.randint(0, max - 1)
@@ -10,7 +11,11 @@ row = 100
 columns = 100
 obstacles_rate = 0.08
 iterations = 30
-results = []
+resultsDFS = []
+resultsBFS = []
+resultsUCS = []
+resultsDLS = []
+
 
 for i in range(0,iterations):
     startX = randomPos(row)
@@ -22,9 +27,22 @@ for i in range(0,iterations):
         goalY = randomPos(columns)
     
     env = Environment(row,columns,startX,startY,goalX,goalY,obstacles_rate)
-    print(f"BFS: {bfs(env,env.start, env.goal)}")
-    print(f"DFS: {dfs(env,env.start, env.goal)}")
-    print(f"UCS: {ucs(env,env.start, env.goal)}")
+    resultsBFS.append(bfs(env,env.start, env.goal))
+    resultsDFS.append(dfs(env,env.start, env.goal))
+    resultsUCS.append(ucs(env,env.start, env.goal))
     limit = math.ceil(len(env.grid)*len(env.grid[0])/2)
-    print(f"DLS: {dls(env,env.start, env.goal, limit)}")
+    result=dls(env,env.start, env.goal, limit)
+    if result != None:
+        resultsDLS.append(result)
+
+algorithms = ["BFS", "DFS", "UCS", "DLS"]
+
+all_results = [resultsBFS, resultsDFS, resultsUCS, resultsDLS ]
+
+plt.boxplot(all_results, labels=algorithms)
+plt.ylabel('Número de pasos')
+plt.title('Comparación de Algoritmos de Búsqueda')
+plt.savefig('boxplot_resultados.png')
+
+
 
